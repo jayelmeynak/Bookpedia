5f4a69031441755f248843c1604ccc548464bca4
jayelmeynak <magomed110301@yandex.ru>
2025-02-02 19:56:17 +0300

    refactor

--- composeApp/src/commonMain/kotlin/com/plcoding/bookpedia/presentation/book_list/BookListState.kt
+++ composeApp/src/commonMain/kotlin/com/plcoding/bookpedia/presentation/book_list/BookListState.kt
@@ -6,7 +6,7 @@ import com.plcoding.bookpedia.domain.Book
 data class BookListState(
     val searchQuery: String = "",
     val searchResults: List<Book> = emptyList(),
-    val favouriteBooks: List<Book> = emptyList(),
+    val favoriteBooks: List<Book> = emptyList(),
     val isLoading:Boolean = false,
     val selectedTabIndex: Int = 0,
     val errorMessage: UiText? = null,
