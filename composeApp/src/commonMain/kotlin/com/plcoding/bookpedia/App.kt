3708126b042ad783b844bd22917d3ab59edd6c82
jayelmeynak <magomed110301@yandex.ru>
2025-02-02 20:00:25 +0300

    feat(commonMain): реализован экран BookListScreen и создана viewModel

--- composeApp/src/commonMain/kotlin/com/plcoding/bookpedia/App.kt
+++ composeApp/src/commonMain/kotlin/com/plcoding/bookpedia/App.kt
@@ -1,10 +1,15 @@
 package com.plcoding.bookpedia
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import com.plcoding.bookpedia.presentation.book_list.BookListScreenRoot
+import com.plcoding.bookpedia.presentation.book_list.BookListViewModel
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
 @Composable
 @Preview
 fun App() {
-
+    BookListScreenRoot(
+        viewModel = remember { BookListViewModel() },
+    ) {  }
 }
