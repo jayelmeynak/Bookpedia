232c6bd74a674887d45a488e4f54593926b8077a
jayelmeynak <magomed110301@yandex.ru>
2025-02-02 15:30:46 +0300

    feat(commonMain/domain): create data class Book

---
+++ composeApp/src/commonMain/kotlin/com/plcoding/bookpedia/domain/Book.kt
@@ -0,0 +1,16 @@
+package com.plcoding.bookpedia.domain
+
+data class Book(
+    val id: String,
+    val title: String,
+    val imageUrl: String,
+    val authors: List<String>,
+    val description: String?,
+    val language: List<String>,
+    val firstPublishYear: String?,
+    val averageRating: Double?,
+    val ratingCount: Int?,
+    val numPages: Int?,
+    val numEditions: Int,
+
+)
