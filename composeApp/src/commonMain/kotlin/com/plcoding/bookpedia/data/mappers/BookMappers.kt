bbeb9ce48ae6dde845fb2caea21182abaf0d9231
jayelmeynak <magomed110301@yandex.ru>
2025-02-02 21:45:28 +0300

    feat(dto): создание SearchResponseDto, SearchedBookDto и extension-функции для маппинга в Book

---
+++ composeApp/src/commonMain/kotlin/com/plcoding/bookpedia/data/mappers/BookMappers.kt
@@ -0,0 +1,24 @@
+package com.plcoding.bookpedia.data.mappers
+
+import com.plcoding.bookpedia.data.dto.SearchedBookDto
+import com.plcoding.bookpedia.domain.Book
+
+fun SearchedBookDto.toBook(): Book {
+    return Book(
+        id = id.substringAfterLast("/"),
+        title = title,
+        imageUrl = if(coverKey != null) {
+            "https://covers.openlibrary.org/b/olid/${coverKey}-L.jpg"
+        } else {
+            "https://covers.openlibrary.org/b/id/${coverAlternativeKey}-L.jpg"
+        },
+        authors = authorNames ?: emptyList(),
+        description = null,
+        language = languages ?: emptyList(),
+        firstPublishYear = firstPublishYear.toString(),
+        averageRating = ratingsAverage,
+        ratingCount = ratingsCount,
+        numPages = numPagesMedian,
+        numEditions = numEditions ?: 0
+    )
+}
