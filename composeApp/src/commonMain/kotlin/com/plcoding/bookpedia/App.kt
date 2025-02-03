9d235995a7f05e08296dad60239b81ba022a4427
jayelmeynak <magomed110301@yandex.ru>
2025-02-03 05:15:00 +0300

    di

--- composeApp/src/commonMain/kotlin/com/plcoding/bookpedia/App.kt
+++ composeApp/src/commonMain/kotlin/com/plcoding/bookpedia/App.kt
@@ -1,15 +1,19 @@
 package com.plcoding.bookpedia
 
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.remember
 import com.plcoding.bookpedia.presentation.book_list.BookListScreenRoot
 import com.plcoding.bookpedia.presentation.book_list.BookListViewModel
 import org.jetbrains.compose.ui.tooling.preview.Preview
+import org.koin.compose.viewmodel.koinViewModel
 
 @Composable
 @Preview
 fun App() {
+    val viewModel = koinViewModel<BookListViewModel>()
     BookListScreenRoot(
-        viewModel = remember { BookListViewModel() },
-    ) {  }
+        viewModel = viewModel,
+        onBookClick = {
+
+        }
+    )
 }
---
+++ composeApp/src/commonMain/kotlin/com/plcoding/bookpedia/di/Modules.kt
@@ -0,0 +1,25 @@
+package com.plcoding.bookpedia.di
+
+import com.plcoding.bookpedia.core.data.HttpClientFactory
+import com.plcoding.bookpedia.data.remote.KtorRemoteBookDataSource
+import com.plcoding.bookpedia.data.remote.RemoteBookDataSource
+import com.plcoding.bookpedia.data.repository.DefaultBookRepository
+import com.plcoding.bookpedia.domain.BookRepository
+import com.plcoding.bookpedia.presentation.book_list.BookListViewModel
+import org.koin.core.module.Module
+import org.koin.core.module.dsl.singleOf
+import org.koin.core.module.dsl.viewModelOf
+import org.koin.dsl.bind
+import org.koin.dsl.module
+
+expect val platformModule: Module
+
+val sharedModule = module {
+    single {
+        HttpClientFactory.create(get())
+    }
+    singleOf(::KtorRemoteBookDataSource).bind<RemoteBookDataSource>()
+    singleOf(::DefaultBookRepository).bind<BookRepository>()
+    viewModelOf(::BookListViewModel)
+
+}
