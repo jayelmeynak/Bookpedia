80470d10d70d19d89ff6246ca7abdab2edb10aef
jayelmeynak <magomed110301@yandex.ru>
2025-02-04 02:39:42 +0300

    feat(domain): добавление получения деталей книги в BookRepository

--- composeApp/src/commonMain/kotlin/com/plcoding/bookpedia/domain/BookRepository.kt
+++ composeApp/src/commonMain/kotlin/com/plcoding/bookpedia/domain/BookRepository.kt
@@ -5,4 +5,6 @@ import com.plcoding.bookpedia.core.domain.Result
 
 interface BookRepository {
     suspend fun searchBooks(query: String): Result<List<Book>, DataError.RemoteError>
+
+    suspend fun getBookDetails(bookId: String): Result<String?, DataError>
 }
